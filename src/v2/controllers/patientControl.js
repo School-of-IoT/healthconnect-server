e43f4b5f23411ebfc8c03153f3fceb12ea8e83f8
Akshayan Sinha <akshayan.sinha@gmail.com>
2025-05-10 03:54:19 +0530

    fix: updatehealthdata token issue

--- src/v2/controllers/patientControl.js
+++ src/v2/controllers/patientControl.js
@@ -240,7 +240,7 @@ const updateHealthData = async (req, res) => {
   try {
       const { _id } = req.params;
       const { healthData } = req.body;
-      const token = req.headers.['token'];
+      const token = req.headers['token'];
       console.log("Token->", token);
   
       if (!healthData || Object.keys(healthData).length === 0) {
